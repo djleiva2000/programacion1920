9fba0703ccbafe2fbb0bb9354be8639f995f93d8
unknown <diegoleiva140@gmail.com>
2019-10-18 19:33:11 -0500

    cambios realizados

---
+++ JavaApplication1/src/javaapplication1/operadores.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ /*Esta clase va a sevir para trabajar con distintas clases para trabajar con operadores*/
+package javaapplication1;
+
+/**
+ *
+ * @author Salas
+ */
+public class operadores {
+    public static void main(String[] args) {
+        //operadores aritmeticos
+        int suma = 14 + 14;
+        int resta = 20 - 10;
+        double multiplicacion = 50 * 0.12;
+        int division = 100 / 50;
+        int residuo = 7 % 2;
+        
+        System.out.println("La sume es: " + suma);
+        System.out.println("la resta es: " + resta);
+        System.out.println("la multiplicacion es: " + multiplicacion);
+        System.out.println("la division es: "+ division);
+        System.out.println("el residuo es: " + residuo);
+        
+        // operadores de incremento o decremento
+        
+    }
+    
+}
