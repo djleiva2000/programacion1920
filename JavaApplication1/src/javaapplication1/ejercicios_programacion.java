f93c6891a15126d4b08aa2aa1485bd2f8f26360a
djleiva2000 <diegoleiva140@gmail.com>
2019-10-31 18:27:23 -0500

    deber 2

---
+++ JavaApplication1/src/javaapplication1/ejercicios_programacion.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author diegoleivaespin
+ */
+public class ejercicios_programacion {
+
+  // primer ejercicio
+         Scanner obj = new Scanner(System.in);
+        /* System.out.println("PRECIO DE UN TERRENO");
+         double largo;
+         double ancho;
+         double precio;
+         double metros;
+         double pre_t;
+         double pre_d = 0;
+         double desc = 0.90;
+         System.out.println("");
+
+         System.out.println("ingrese el largo del terreno");
+         largo = obj.nextDouble();
+
+         System.out.println("");
+
+         System.out.println("ingrese el ancho del terreno");
+         ancho = obj.nextDouble();
+
+         System.out.println("ingrese el precio");
+         precio = obj.nextDouble();
+
+         metros = largo * ancho;
+         System.out.println("");
+         pre_t = metros * precio;
+
+         if (pre_t >= 400) {
+             pre_d = pre_t * desc;
+  }
+                 System.out.println("el costo del terreno es:" + pre_d);
+                 else
+               System.out.println("el precio del terreno es:" +pre_t); */
+
+
+
+         /*System.out.println("el valor para x");
+         double x;
+         double a;
+         double tot;
+         System.out.println("ingrese el calor");
+         x = obj.nextDouble();
+
+         if (x < 0) {
+         }
+             a = 3 * x + 6;
+             System.out.println("el valor de a es:"+a);
+          if (x >= 0) {
+        }
+              a = x * x + 6;
+              System.out.println("el valor de a es:"+a);*/
+
+        /* System.out.println("PRECIO DEL PEDIDIO");
+         double cant;
+         double precio;
+         double pedido;
+         double pre_t;
+         System.out.println("ingrese cantidad");
+         cant = obj.nextDouble();
+         System.out.println("ingrese precio");
+         precio = obj.nextDouble();
+         if (cant > precio)
+             pedido = (precio * 0.85) * cant;
+         System.out.println("precio con descuento es:" +pedido);
+         else
+             pre_t = cant * precio;
+         System.out.println("el precio es:" +pre_t);*/
+
+
+
+        /* System.out.println("PAGO EN EFCETIVO O CREDITO");
+        double forma_pago;
+         double hoja;
+         double precio1;
+         double precio2;
+         double precio3;
+         double viguetas;
+         double armazones;
+
+         double cos_total1;
+         double cos_total;
+         double cant3;
+         double cant2;
+         double cant1;
+         System.out.println("pagar en EFECTIVO PRESIONE (1) si paga en credito presione (2)");
+         System.out.println("elija su forma de pago");
+         forma_pago = obj.nextDouble();
+         System.out.println("cantidad hojas");
+         hoja = obj.nextDouble();
+         System.out.println("costo hojas");
+         precio1 = obj.nextDouble();
+         System.out.println("cantidad viguetas");
+         viguetas = obj.nextDouble();
+         System.out.println("costo viguetas");
+         precio2 = obj.nextDouble();
+         System.out.println("cantidad armazones");
+         armazones = obj.nextDouble();
+         System.out.println("costo aramazones");
+         precio3 = obj.nextDouble();
+
+         if (forma_pago == 2)
+             cant1 = (hoja * precio1) * 0.80;
+         System.out.println("costo hojas es:"+cant1);
+         cant2 = (viguetas * cost2) * 0.85;
+         System.out.println("costo viguetas es :"+cant2);
+         cant3 = (armazones * cost3);
+         System.out.println("costo armazones es:");
+         cos_total1 = cant1 + cant2 + cant3;
+         System.out.println("su pago en credito es :"+cos_total1);
+
+
+         if (forma_pago == 1)
+             cant1 = (hoja * precio1) * 0.80;
+         System.out.println("costo hojas es:"+cant1);
+         cant2 = (viguetas * cost2) * 0.85;
+         System.out.println("costo viguetas es :"+cant2);
+         cant3 = (armazones * cost3);
+         System.out.println("costo armazones es:");
+         cos_total1 = (cant1 + cant2 + cant3) * 0.97;
+         System.out.println("su pago en credito es :"+cos_total);*/
+
+
+        /*
+         System.out.println("NOTAS");
+         double nomb;
+         double n1;
+         double n2;
+         double n3;
+         double promedio;
+
+         System.out.println("ingrese nota 1");
+         n1 = obj.nextDouble();
+         System.out.println(" ingrese nota 2");
+         n2 = obj.nextDouble();
+         System.out.println("ingrese nota 3");
+         n3 = obj.nextDouble();
+
+         if (n1 >= 70)
+             if (n2 >= 70)
+                 if (n3 >= 70)
+                 promedio = (n1+n2+n3)/3;
+                 System.out.println("su promedio es :"+promedio);
+                 else
+                 System.out.println("no alcansa");
+                 else System.out.println("no alcansa");
+                 System.out.println("no alcansa");*/
+
+
+        /* System.out.println("ANGULOS");
+         double A;
+         double B;
+         double C;
+
+         System.out.println("valor a");
+         A = obj.nextDouble();
+         System.out.println("valor b");
+         B = obj.nextDouble();
+         System.out.println("valor c");
+         C = obj.nextDouble();
+         if (C*C != A*A + B*B)
+             if (C*C > A*A + B*B)
+                 System.out.println("es obtusangulo:");
+             else
+                 System.out.println("es adiytangulo");
+         else
+             System.out.println("es nectagulo");*/
+
+
+
+
+
+
+     }
+     }
